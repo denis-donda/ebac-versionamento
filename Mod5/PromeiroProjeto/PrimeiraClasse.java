534b37e42b4b2165c671f40e4a84f4066b5d53b3
Dênis Henrique <denishenriquedonda@gmail.com>
2023-06-01 23:21:16 -0300

    Delete PrimeiraClasse.java

--- Mod5/PromeiroProjeto/PrimeiraClasse.java
+++
@@ -1,8 +0,0 @@
-package Mod5.PromeiroProjeto;
-
-public class PrimeiraClasse {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Dênis Henrique Donda de Oliveira");
-    }
-}
