015de6859dbf87c7a96a59989bfa69333cd413dd
denis-donda <denishenriquedonda@gmail.com>
2024-02-23 00:08:57 -0300

    Mod 7 - Tarefa cauculo media

---
+++ Modulo8/tarefa_calculo_media/main.java
@@ -0,0 +1,16 @@
+package Modulo8.tarefa_calculo_media;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        float nota1 = 9.5f;
+        float nota2 = 8.2f;
+        float nota3 = 7.8f;
+        float nota4 = 6.1f;
+
+        float media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
